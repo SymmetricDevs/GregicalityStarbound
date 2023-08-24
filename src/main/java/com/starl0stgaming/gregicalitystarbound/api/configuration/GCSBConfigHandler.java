2fc5deedf75cfacd0ce648949a600be59eeebf56
StarL0st <hector.hals@outlook.com>
2023-08-24 11:33:38 -0700

    very basic work on the config system

--- src/main/java/com/starl0stgaming/gregicalitystarbound/api/configuration/GCSBConfigHandler.java
+++ src/main/java/com/starl0stgaming/gregicalitystarbound/api/configuration/GCSBConfigHandler.java
@@ -4,22 +4,37 @@ import net.minecraftforge.fml.common.Loader;
 import sun.misc.GC;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 
 public class GCSBConfigHandler {
 
 
 
-    private Path configDirPath = Loader.instance().getConfigDir().toPath();
-
     private File GCSBConfigFolder = new File(Loader.instance().getConfigDir(), "gregicalitystarbound");
+
+    private File planetConfigFile = new File(GCSBConfigFolder, "planets.json");
+    private File solarSystemConfigFile = new File(GCSBConfigFolder, "solarsystems.json");
 
 
     public void init() {
         try {
             GCSBConfigFolder.mkdirs();
-        } catch (Exception e) {
+
+            this.initializeConfigFiles();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    private void initializeConfigFiles() throws IOException {
+        try {
+            planetConfigFile.createNewFile();
+            solarSystemConfigFile.createNewFile();
+        } catch (IOException e) {
             throw e;
         }
     }
+
+
 }
