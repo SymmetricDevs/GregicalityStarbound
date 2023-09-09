0a71bfac099b3df8415f5230df6c41786401dabf
Super <52500795+Supernoobv@users.noreply.github.com>
2023-09-09 18:09:53 -0300

    # What
    Fixed dim registration not supporting modded blocks
    
    # Implementation
    Dim/Planet registration now happens during biome registry

--- src/main/java/com/starl0stgaming/gregicalitystarbound/GregicalityStarbound.java
+++ src/main/java/com/starl0stgaming/gregicalitystarbound/GregicalityStarbound.java
@@ -4,14 +4,16 @@ import com.starl0stgaming.gregicalitystarbound.api.configuration.GCSBConfigHandl
 import com.starl0stgaming.gregicalitystarbound.api.configuration.space.SpaceConfigHandler;
 import com.starl0stgaming.gregicalitystarbound.common.CommonProxy;
 import com.starl0stgaming.gregicalitystarbound.common.space.SpaceController;
+import gregtech.GTInternalTags;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.lwjgl.input.Keyboard;
 
-@Mod(name = GregicalityStarbound.NAME, modid = GregicalityStarbound.MODID)
+@Mod(name = GregicalityStarbound.NAME, modid = GregicalityStarbound.MODID, dependencies = GTInternalTags.DEP_VERSION_STRING)
 public class GregicalityStarbound {
 
 
@@ -39,8 +41,10 @@ public class GregicalityStarbound {
         keyBinding = new KeyBinding("key.gregicalitystarbound.test", Keyboard.KEY_P,"key.gregicalitystarbound.category");
 
         ClientRegistry.registerKeyBinding(keyBinding);
+    }
 
-
-
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        proxy.load();
     }
 }
