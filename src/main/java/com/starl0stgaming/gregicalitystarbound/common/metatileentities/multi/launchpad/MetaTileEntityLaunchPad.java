8a1f17cb19f86872ab45ee5634e93ad6bd624355
StarL0st <hector.hals@outlook.com>
2024-01-29 21:40:58 -0700

    -Introduced a slightly more complicated structure for the launch pad, will do for now (although it still needs work) -Introduced a recipe map system for the launch pad, for when that's needed -Some other small things, too

--- src/main/java/com/starl0stgaming/gregicalitystarbound/common/metatileentities/multi/launchpad/MetaTileEntityLaunchPad.java
+++ src/main/java/com/starl0stgaming/gregicalitystarbound/common/metatileentities/multi/launchpad/MetaTileEntityLaunchPad.java
@@ -1,25 +1,34 @@
 package com.starl0stgaming.gregicalitystarbound.common.metatileentities.multi.launchpad;
+import com.starl0stgaming.gregicalitystarbound.api.recipes.GCSBRecipeMaps;
+import gregtech.api.capability.impl.FluidTankList;
+import gregtech.api.capability.impl.MultiblockFuelRecipeLogic;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.metatileentity.multiblock.IMultiblockPart;
+import gregtech.api.metatileentity.multiblock.MultiblockAbility;
 import gregtech.api.metatileentity.multiblock.MultiblockWithDisplayBase;
+import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.unification.material.Materials;
 import gregtech.client.renderer.ICubeRenderer;
 import gregtech.client.renderer.texture.Textures;
 import gregtech.common.blocks.BlockAsphalt;
 import gregtech.common.blocks.MetaBlocks;
 import gregtech.common.blocks.StoneVariantBlock;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nonnull;
 
-public class MetaTileEntityLaunchPad extends MultiblockWithDisplayBase {
+public class MetaTileEntityLaunchPad extends RecipeMapMultiblockController {
 
-    private int width = 5;
-    private int length = 5;
+    // Probably needs a fluid inventory for a water deluge system
+    private FluidTankList inputFluidInventory;
+
     public MetaTileEntityLaunchPad(ResourceLocation mteId) {
-        super(mteId);
+        super(mteId, GCSBRecipeMaps.LAUNCH_PAD_LOADING_RECIPES);
     }
 
     @Override
@@ -27,24 +36,40 @@ public class MetaTileEntityLaunchPad extends MultiblockWithDisplayBase {
         return new MetaTileEntityLaunchPad(this.metaTileEntityId);
     }
 
+    public void initializeAbilities() {
+        this.inputFluidInventory = new FluidTankList(true, getAbilities(MultiblockAbility.IMPORT_FLUIDS));
+    }
+
     @Override
     protected void updateFormedValid() {
-
+        // not necessary for now
     }
 
     @Nonnull
     @Override
     protected BlockPattern createStructurePattern() {
-        return FactoryBlockPattern.start().aisle("XXCXX", "XXXXX", "XXXXX", "XXXXX", "XXXXX"). // Absolutely temporary
-                where('C', selfPredicate()).
-                where('X', states(MetaBlocks.ASPHALT.getState(BlockAsphalt.BlockType.ASPHALT))).
-                where('X', states(MetaBlocks.STONE_BLOCKS.get(StoneVariantBlock.StoneVariant.SMOOTH).getState(StoneVariantBlock.StoneType.CONCRETE_LIGHT))).
-                build();
+        return FactoryBlockPattern.start().aisle("S S S", "S   S", "S   S", "S   S", "S S S")
+                .aisle("XXCXX", "XXXXX", "XXXXX", "XXXXX", "XXXXX")
+                .aisle("  S  ", "     ", "     ", "     ", "     ")
+                .aisle("  S  ", "     ", "     ", "     ", "     ")
+                .aisle("  S  ", "     ", "     ", "     ", "     ")
+                    .where('C', selfPredicate())
+                    .where('X', states(MetaBlocks.STONE_BLOCKS.get(StoneVariantBlock.StoneVariant.SMOOTH).getState(StoneVariantBlock.StoneType.CONCRETE_LIGHT)))
+                    .where('S', states(MetaBlocks.FRAMES.get(Materials.Steel).getBlock(Materials.Steel)).or(autoAbilities()))
+                    .where(' ', air())
+                .build();
     }
 
     @Override
     public ICubeRenderer getBaseTexture(IMultiblockPart iMultiblockPart) {
         return Textures.SOLID_STEEL_CASING;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Nonnull
+    @Override
+    protected ICubeRenderer getFrontOverlay() {
+        return Textures.MACHINE_CONTROLLER_OVERLAY;
     }
 
     @Override
