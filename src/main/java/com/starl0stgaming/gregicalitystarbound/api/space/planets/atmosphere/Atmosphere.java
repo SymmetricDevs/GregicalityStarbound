b4b607ff3acf34c4d44c0596fa2a2ce2bd07e954
StarL0st <hector.hals@outlook.com>
2024-07-27 12:50:39 -0700

    geckolib dependency

--- src/main/java/com/starl0stgaming/gregicalitystarbound/api/space/planets/atmosphere/Atmosphere.java
+++ src/main/java/com/starl0stgaming/gregicalitystarbound/api/space/planets/atmosphere/Atmosphere.java
@@ -3,8 +3,6 @@ package com.starl0stgaming.gregicalitystarbound.api.space.planets.atmosphere;
 public class Atmosphere {
 
     public int atmosphericPressure;
-
-
     public int waterColor;
     public int skyColor;
 
